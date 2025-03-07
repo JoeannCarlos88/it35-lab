3a0fdbf870b089d2218e5f5cb08975cb1a3f1a03
JoeannCarlos88 <joeanncarlos@gmail.com>
2025-03-07 21:31:08 +0800

    added some text in fav,feed and search

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,38 +1,58 @@
-import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            Feed
-          </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Feed;
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonAccordion,
+  IonAccordionGroup
+} from '@ionic/react';
+import { star } from 'ionicons/icons';
+
+const dresses = [
+  { name: 'Cocktail Dress', sizes: 'S, M, L, XL' },
+  { name: 'Evening Gown', sizes: 'M, L, XL' },
+  { name: 'Maxi Dress', sizes: 'S, M, L' },
+  { name: 'Bodycon Dress', sizes: 'XS, S, M, L' },
+  { name: 'A-line Dress', sizes: 'S, M, L, XL' }
+];
+
+const Feed: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonList>
+          <IonAccordionGroup>
+            {dresses.map((dress, index) => (
+              <IonAccordion key={index} value={dress.name}>
+                <IonItem slot="header">
+                  <IonLabel>{dress.name}</IonLabel>
+                  <IonIcon icon={star} color="warning" slot="end" />
+                </IonItem>
+                <div slot="content" style={{ padding: '10px' }}>
+                  Available Sizes: {dress.sizes}
+                </div>
+              </IonAccordion>
+            ))}
+          </IonAccordionGroup>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Feed;
--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,39 +1,50 @@
-import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Search: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Search</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            Search
-          </div>
-  
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Search;
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonSearchbar
+} from '@ionic/react';
+
+const Search: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Search</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonSearchbar placeholder="Search for a dress..." />
+        <IonList>
+          <IonItem>
+            <IonLabel>Casual Dress</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Evening Gown</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Cocktail Dress</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Maxi Dress</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Bodycon Dress</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Search;
