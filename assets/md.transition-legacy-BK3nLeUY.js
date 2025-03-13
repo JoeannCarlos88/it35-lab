e5bac080a5ef2504c14e75d88c45e1aa36b4b3a0
joeanncarlos88 <joeanncarlos@gmail.com>
2025-03-13 08:48:19 +0800

    Updates

--- assets/md.transition-legacy-BK3nLeUY.js
+++ assets/md.transition-legacy-BK3nLeUY.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Brw86AmN.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-3HHTDvtX.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
