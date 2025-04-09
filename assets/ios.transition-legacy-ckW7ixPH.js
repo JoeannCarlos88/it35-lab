22b77da3ec963b1c6dae5f2d4cdcac8f1d6c8a93
JoeannCarlos88 <joeanncarlos@gmail.com>
2025-04-09 22:36:42 +0800

    Updates

--- assets/ios.transition-legacy-ckW7ixPH.js
+++ assets/ios.transition-legacy-ckW7ixPH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-3HHTDvtX.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-D9N_e2Jb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
