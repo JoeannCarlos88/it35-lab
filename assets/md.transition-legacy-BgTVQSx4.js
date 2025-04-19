e45a442fdfc6d7082fa6ce614ca5249743a4ba6c
joeanncarlos88 <joeanncarlos@gmail.com>
2025-04-19 15:55:05 -0700

    Updates

--- assets/md.transition-legacy-BgTVQSx4.js
+++ assets/md.transition-legacy-BgTVQSx4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D9N_e2Jb.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DzXj-pVu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
