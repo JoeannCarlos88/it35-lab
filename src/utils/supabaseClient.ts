e7705a0228ea128a43fdab728c0fb2e4159f1dd1
joeanncarlos88 <joeanncarlos@gmail.com>
2025-04-19 15:34:25 -0700

    Added dynamic alertbox for login and register

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = 'https://hlvbnvaobmfkvbcesfvw.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsdmJudmFvYm1ma3ZiY2VzZnZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMDgxNjUsImV4cCI6MjA1OTc4NDE2NX0.AKIVdHaKsbVDLTJqJd8GoDE1Aqu_IX6LzOsuj-DPO-c';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
