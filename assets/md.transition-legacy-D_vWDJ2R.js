a7ed12371f39680319ee48b7583868999d806aaf
JoeannCarlos88 <joeanncarlos@gmail.com>
2025-05-05 15:31:02 +0800

    Updates

--- assets/md.transition-legacy-D_vWDJ2R.js
+++ assets/md.transition-legacy-D_vWDJ2R.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DzXj-pVu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DG2wp-Yo.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
