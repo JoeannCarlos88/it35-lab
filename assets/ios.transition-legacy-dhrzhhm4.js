95adba7ac888a4fb67eaa018a3343159d92a81ba
JoeannCarlos88 <joeanncarlos@gmail.com>
2025-05-05 15:39:52 +0800

    Updates

--- assets/ios.transition-legacy-dhrzhhm4.js
+++ assets/ios.transition-legacy-dhrzhhm4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DG2wp-Yo.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Cw8mVohN.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
